b7a7bb864f77e4c36dfb03bbb752ffaa5b2fc680
Alexey Lunacharsky <alun@katlex.com>
2015-06-20 00:59:19 +0600

    Added underscore to enable heroku

--- src/oanda_proxy/web.clj
+++ src/oanda_proxy/web.clj
@@ -1,4 +1,4 @@
-(ns oanda-proxy.web
+(ns oanda_proxy.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
